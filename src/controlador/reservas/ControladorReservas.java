a9a0f9af4e5d1f24c0f79fc35fb5bb6a457242b6
rruirey <rruirey2301@g.educaand.es>
2022-05-25 15:46:26 +0200

    stored reservations on local cache, created buttons logic and created render method to update the main panel.

--- src/controlador/reservas/ControladorReservas.java
+++ src/controlador/reservas/ControladorReservas.java
@@ -3,27 +3,66 @@ package controlador.reservas;
 import modelo.dao.reserva.ReservaDAO;
 import modelo.dao.reserva.ReservaDAOImpl;
 import modelo.dao.usuario.Usuario;
-import modelo.dao.usuario.UsuarioDAO;
-import vista.logueo.Login;
 import vista.reservas.Reserva;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControladorReservas {
-    private ReservaDAO modelo;
-    private Reserva vista;
-    private Usuario usuario;
 
-    public ControladorReservas(Usuario usuario) {
-        this.modelo = new ReservaDAOImpl();
-        this.vista = new Reserva();
-        this.usuario = usuario;
-        inicializarVista();
+  private final ReservaDAO modelo;
+  private List<modelo.dao.reserva.Reserva> reservas;
+
+  private final Reserva vista;
+  private final Usuario usuario;
+
+  private int index;
+
+  public ControladorReservas(Usuario usuario) {
+    this.usuario = usuario;
+    this.vista = new Reserva();
+
+    this.modelo = new ReservaDAOImpl();
+    try {
+      reservas = this.modelo.obtenerTodasReservas();
+    } catch (SQLException ex) {
+      reservas = new ArrayList<>();
+      ex.printStackTrace();
     }
 
-    private void inicializarVista() {
-        vista.getVentanaReservas().setVisible(true);
-        vista.getLabelPrueba().setText(usuario.toString());
-    }
-    public void inicializarControlador() {
+    inicializarVista();
+  }
 
+  private void inicializarVista() {
+    vista.getVentanaReservas().setVisible(true);
+  }
+
+  public void inicializarControlador() {
+    vista.getNextPageButton().addActionListener(event -> {
+      handleButtonLogic(1);
+    });
+    vista.getPrevPageButton().addActionListener(event -> {
+      handleButtonLogic(-1);
+    });
+  }
+
+  public void handleButtonLogic(int modifier) {
+    index += modifier;
+
+    if (index > reservas.size()) {
+      index = 0;
     }
+
+    if (index < 0) {
+      index = reservas.size();
+    }
+
+    render();
+  }
+
+  public void render() {
+    // make graphic update
+  }
+
 }
